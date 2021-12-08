34acb3cd76631c57391ff1deaf334cec08d6a2da
ivp.dev <ivp.dev@outlook.com>
2021-12-08 18:20:26 +0300

    replace __dirname with path.resolve

--- src/common/config.ts
+++ src/common/config.ts
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({
-  path: path.join(__dirname, '../../.env')
+  path: path.join(path.resolve(), '../../.env')
 });
 
 const config = {
-  PORT: process.env.PORT || "5000",
+  PORT: process.env.PORT || "4000",
   NODE_ENV: process.env.NODE_ENV,
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
