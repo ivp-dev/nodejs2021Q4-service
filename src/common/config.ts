e692d3cac62e4c9b7adc939d41da7004b5a4e9e8
Vadim I <ivp.dev@outlook.com>
2021-12-10 18:07:53 +0300

    eslint

--- src/common/config.ts
+++ src/common/config.ts
@@ -6,11 +6,11 @@ dotenv.config({
 });
 
 const config = {
-  PORT: process.env["PORT"] || '4000',
-  NODE_ENV: process.env["NODE_ENV"],
-  MONGO_CONNECTION_STRING: process.env["MONGO_CONNECTION_STRING"],
-  JWT_SECRET_KEY: process.env["JWT_SECRET_KEY"],
-  AUTH_MODE: process.env["AUTH_MODE"] === 'true',
+  PORT: process.env.PORT || '4000',
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  AUTH_MODE: process.env.AUTH_MODE === 'true',
 };
 
 export default config;
